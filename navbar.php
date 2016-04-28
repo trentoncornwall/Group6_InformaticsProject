8ef0a2cd264735b622591c4b43e9e9a0a846601e
trentoncornwall <trenton434@gmail.com>
2016-04-28 12:41:07 -0500

    added profile to navbar.php

--- navbar.php
+++ navbar.php
@@ -8,7 +8,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span> 
 				</button>
-				<a class="navbar-brand" href="home.php"><?php echo $_SESSION['username']; ?></a>
+				<a class="navbar-brand" href="profile.php"><?php echo $_SESSION['username']; ?></a>
 			</div>
 			<div class="collapse navbar-collapse" id="myNavbar">
 				<ul class="nav navbar-nav navbar-right">
@@ -16,6 +16,7 @@
 					<li <?php if ($menuActive==1) echo "class='active'"; ?>><a href="jobs.php">JOBS</a></li>
 					<li <?php if ($menuActive==2) echo "class='active'"; ?>><a href="hours.php">HOURS</a></li>
 					<li <?php if ($menuActive==3) echo "class='active'"; ?>><a href="paystub.php">PAY STUBS</a></li>
+					<li <?php if ($menuActive==5) echo "class='active'"; ?>><a href="profile.php">PROFILE</a></li>
 					<li <?php if ($menuActive==4) echo "class='active'"; ?>><a href="logout.php">LOG OUT</a></li>
 				</ul>
 			</div>
