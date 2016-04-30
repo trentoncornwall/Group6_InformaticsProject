35bf6bca9e72ceea962b2492262d2cdee98d16fc
[tcomer17] <[travis-comer@uiowa.edu]>
2016-04-29 20:26:43 -0500

    fixed table

--- hours.php
+++ hours.php
@@ -209,7 +209,7 @@ $PID=$_SESSION['PID'];
 $db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
 
 //Query to populate table with hours and date entered. limit 14 days
-$query = "SELECT DISTINCT Hours, Hours_Date FROM Hours_T, Job_T WHERE Job_T.PID = $PID ORDER BY Hours_Date DESC LIMIT 14;";
+$query = "SELECT Hours, Hours_Date FROM Hours_T WHERE Hours_T.JID = (SELECT JID FROM Job_T WHERE Job_T.PID = $PID) ORDER BY Hours_Date DESC LIMIT 14;";
 $result = queryDB($query,$db);
 if (nTuples($result) > 0) {
     // Creating table
